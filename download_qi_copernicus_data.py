a4fca25eb84b6d8bde129e89fefb063ec4d58df4
kimzed <cedric.baron.ls@gmail.com>
2023-11-04 17:08:30 +0100

    change the data source. refactor functionalities in a function

--- download_qi_copernicus_data.py
+++ download_qi_copernicus_data.py
@@ -2,68 +2,89 @@ import ee
 import geopandas as gpd
 import pandas as pd
 from shapely.ops import unary_union
-from utils import get_daily_temperature_mean, get_daily_precipitation_mean
+from pathlib import Path
+from utils import get_spatial_mean
+from pandas import DataFrame
+
+MODEL_NASA = "CNRM-ESM2-1"
 
 
-ee.Initialize()
+def generate_time_series(
+    image_collection_id: str,
+    band: str,
+    basin_geo_file: Path,
+    start_date: str,
+    end_date: str,
+) -> DataFrame:
+    # Initialize the Earth Engine API.
+    ee.Initialize()
 
-basin_gdf = gpd.read_file("/home/cedric/repos/cassini_data/naussac_water_bassin.gpkg")
-basin_polygon = basin_gdf.geometry.to_list()[0]
-merged_polygon = unary_union(basin_polygon)
-coords_water_basin = list(merged_polygon.exterior.coords)
+    basin_gdf = gpd.read_file(basin_geo_file)
+    basin_polygon = basin_gdf.geometry.to_list()[0]
+    merged_polygon = unary_union(basin_polygon)
+    coords_water_basin = list(merged_polygon.exterior.coords)
 
-ee_water_basin_polygon = ee.Geometry.Polygon(coords_water_basin)
+    ee_water_basin_polygon = ee.Geometry.Polygon(coords_water_basin)
 
-# Load the ERA5 daily temperature at 2 meters dataset.
-era5_daily_temp = ee.ImageCollection("ECMWF/ERA5/DAILY")
+    image_collection = ee.ImageCollection(image_collection_id)
 
-# construction of the dam starts in 1976. ends up in 1980
-start_date = "2000-01-01"
-end_date = "2001-01-01"
+    if image_collection_id == "NASA/GDDP-CMIP6":
+        filtered_collection = (
+            image_collection.filterBounds(ee_water_basin_polygon).filterDate(
+                start_date, end_date
+            )
+        ).filter(ee.Filter.eq("model", MODEL_NASA))
+    else:
+        filtered_collection = image_collection.filterBounds(
+            ee_water_basin_polygon
+        ).filterDate(start_date, end_date)
 
-temp_filtered = era5_daily_temp.filterBounds(ee_water_basin_polygon).filterDate(
-    start_date, end_date
-)
-
-# Map the function over the image collection.
-daily_mean_temp = temp_filtered.map(
-    lambda image: get_daily_temperature_mean(image, ee_water_basin_polygon)
-)
-daily_mean_precipitation = temp_filtered.map(
-    lambda image: get_daily_precipitation_mean(image, ee_water_basin_polygon)
-)
-
-time_series_temp = (
-    daily_mean_temp.reduceColumns(
-        ee.Reducer.toList(2), ["system:time_start", "daily_mean_temp"]
+    daily_mean = filtered_collection.map(
+        lambda image: get_spatial_mean(image, ee_water_basin_polygon, band)
     )
-    .values()
-    .get(0)
-)
-time_series_precipitation = (
-    daily_mean_precipitation.reduceColumns(
-        ee.Reducer.toList(2), ["system:time_start", "daily_mean_precipitation"]
+
+    # Reduce the collections to lists
+    time_series = (
+        daily_mean.reduceColumns(
+            ee.Reducer.toList(2), ["system:time_start", "daily_mean_temp"]
+        )
+        .values()
+        .get(0)
     )
-    .values()
-    .get(0)
+
+    # Get the results as a Python list.
+    values = time_series.getInfo()
+
+    df_time_series = pd.DataFrame(values, columns=["timestamp", "mean_temp"])
+    df_time_series["timestamp"] = pd.to_datetime(df_time_series["timestamp"], unit="ms")
+    df_time_series.set_index("timestamp", inplace=True)
+
+    return df_time_series
+
+
+path_water_basin = Path("/home/cedric/repos/cassini_data/villeret_water_basin.gpkg")
+
+precipitation_time_series = generate_time_series(
+    "NASA/GDDP-CMIP6",
+    "pr",
+    path_water_basin,
+    "2000-01-01",
+    "2001-01-01",
+)
+
+temperature_time_series = generate_time_series(
+    "NASA/GDDP-CMIP6",
+    "tas",
+    path_water_basin,
+    "2000-01-01",
+    "2001-01-01",
 )
 
 
-# Get the results as a Python list.
-values_temperature = time_series_temp.getInfo()
-values_precipitation = time_series_precipitation.getInfo()
-
-# min max for the temperature is 223.6-304, in kelvin.
-# conversion is: temp=kelvin-272.15
-time_series_temp = pd.DataFrame(values_temperature, columns=["timestamp", "mean_temp"])
-time_series_temp["timestamp"] = pd.to_datetime(time_series_temp["timestamp"], unit="ms")
-time_series_temp.set_index("timestamp", inplace=True)
-
-# min max for the precipitation is 0 - 0.02
-time_series_precipitation = pd.DataFrame(
-    values_precipitation, columns=["timestamp", "mean_precipitation"]
+evapotranspiration = generate_time_series(
+    "ECMWF/ERA5_LAND/DAILY_AGGR",
+    "potential_evaporation_sum",
+    path_water_basin,
+    "2000-01-01",
+    "2001-01-01",
 )
-time_series_precipitation["timestamp"] = pd.to_datetime(
-    time_series_precipitation["timestamp"], unit="ms"
-)
-time_series_precipitation.set_index("timestamp", inplace=True)
--- utils.py
+++ utils.py
@@ -4,20 +4,16 @@ from ee.geometry import Geometry
 from ee.imagecollection import ImageCollection
 
 
-def get_daily_temperature_mean(
-    image: ImageCollection, polygon: Geometry
+def get_spatial_mean(
+    image: ImageCollection,
+    polygon: Geometry,
+    band: str,
 ) -> ImageCollection:
+    # we set best effort to false to prevent issue with max number of pixels
     mean = image.reduceRegion(
-        reducer=ee.Reducer.mean(), geometry=polygon, scale=30
-    ).get("mean_2m_air_temperature")
+        reducer=ee.Reducer.mean(),
+        geometry=polygon,
+        scale=30,
+        bestEffort=False,
+    ).get(band)
     return image.set("daily_mean_temp", mean)
-
-
-# Define a function that takes an image and returns the mean of the image over the polygon.
-def get_daily_precipitation_mean(
-    image: ImageCollection, polygon: Geometry
-) -> ImageCollection:
-    mean = image.reduceRegion(
-        reducer=ee.Reducer.mean(), geometry=polygon, scale=30
-    ).get("total_precipitation")
-    return image.set("daily_mean_precipitation", mean)
